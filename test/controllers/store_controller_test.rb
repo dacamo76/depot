ae04735deac82dd98dda9b8a1273100e6e2415fa
Daniel Cañas <daniel_76@yahoo.com>
2013-07-09 14:34:16 -0600

    Fixes failing tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -16,10 +16,16 @@ class StoreControllerTest < ActionController::TestCase
     refute session[:count].nil?
     assert session[:count] == 1
     get :index
-    assert session[:count] == 2    
+    assert session[:count] == 2
     assert_difference('session[:count]', 2) do
       get :index
       get :index
     end
+  end
+
+  test "markup needed for store.js.coffee is in place" do
+    get :index
+    assert_select '.store .entry > img', 3
+    assert_select '.entry input[type=submit]', 3
   end
 end
